b88163d13c35633bfdc97699af9bc043654fe757
Isaiah Mann <isaiahbmann@gmail.com>
2025-03-22 06:51:15 -0700

    Fix dark mode for text

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -8,8 +8,8 @@ export const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
   return (
     <div className="mb-8">
       <div className="flex justify-between items-center mb-2">
-        <h2 className="text-xl font-semibold text-white">Your Progress</h2>
-        <span className="text-xl font-bold text-white">{progress}%</span>
+        <h2 className="text-xl font-semibold dark:text-white text-black">Your Progress</h2>
+        <span className="text-xl font-bold dark:text-white text-black">{progress}%</span>
       </div>
       <div className="progress-bar">
         <div
